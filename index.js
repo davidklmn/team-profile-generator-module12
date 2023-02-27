0016374c58eb410111d3e935510d57216f78a826
davidklmn <davidkelemen88@gmail.com>
2023-02-27 19:19:00 +0000

    Prompts for each employee and prompt questions for next employee function added

--- index.js
+++ index.js
@@ -9,17 +9,54 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
+const Employee = require("./lib/Employee");
+
+// team array
+const team = [];
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 inquirer
   .prompt([
+    // manager questions
     {
-      //manager questions
+      type: "input",
+      message: "Team Manager's name: ",
+      name: "managerName",
+    },
+    {
+      type: "input",
+      message: "Team Manager's employee number: ",
+      name: "managerId",
+    },
+    {
+      type: "input",
+      message: "Team Manager's email address: ",
+      name: "managerEmail",
+    },
+    {
+      type: "input",
+      message: "Team Manager's office number: ",
+      name: "managerOffice",
     },
   ])
   .then((response) => {
     // populate manager info
-    // promptForNexEmployee ()
+    const managerName = `${response.managerName}`;
+    const managerId = `${response.managerId}`;
+    const managerEmail = `${response.managerEmail}`;
+    const managerOffice = `${response.managerOffice}`;
+
+    // generate manager class
+    const manager = new Manager(
+      managerName,
+      managerId,
+      managerEmail,
+      managerOffice
+    );
+    // push manager into the array of employees
+    team.push(manager);
+    // calling function for the next user input
+    promptForNextEmployee();
   });
 
 const promptForNextEmployee = () => {
@@ -27,15 +64,31 @@ const promptForNextEmployee = () => {
     .prompt([
       {
         // choice of 3
+        type: "list",
+        message: "What's next?",
+        choices: [
+          "Add an engineer",
+          "Add an intern",
+          "Finish building the team",
+        ],
+        name: "choices",
       },
     ])
     .then((response) => {
       // if engineer
-      //    promptForEngineer
-      // else if intern
-      //    promptForIntern
-      // else
-      //    use the functionality from page-template to generate the team
+      if (response.choices === "Add an engineer") {
+        //    promptForEngineer
+        promptForEngineer();
+        // else if intern
+      } else if (response.choices === "Add an intern") {
+        //    promptForIntern
+        promptForIntern();
+        // else
+      } else {
+        //    use the functionality from page-template to generate the team
+        // buildPage(team);
+        console.log("done");
+      }
     });
 };
 
@@ -44,11 +97,37 @@ const promptForEngineer = () => {
     .prompt([
       {
         //engineer questions
+        type: "input",
+        message: "Engineer's name: ",
+        name: "engName",
+      },
+      {
+        type: "input",
+        message: "Engineer's id: ",
+        name: "engid",
+      },
+      {
+        type: "input",
+        message: "Engineer's email: ",
+        name: "engEmail",
+      },
+      {
+        type: "input",
+        message: "Engineer's GitHub username: ",
+        name: "engGit",
       },
     ])
     .then((response) => {
+      const engName = `${response.engName}`;
+      const engId = `${response.engId}`;
+      const engEmail = `${response.engEmail}`;
+      const engGit = `${response.engGit}`;
+
+      const engineer = new Engineer(engName, engId, engEmail, engGit);
       // add new engineer to employees array
-      // promptForNextEmployee
+      team.push(engineer);
+
+      promptForNextEmployee();
     });
 };
 
@@ -57,14 +136,44 @@ const promptForIntern = () => {
     .prompt([
       {
         //intern questions
+        type: "input",
+        message: "Intern's name: ",
+        name: "intName",
+      },
+      {
+        //intern questions
+        type: "input",
+        message: "Intern's id: ",
+        name: "intId",
+      },
+      {
+        //intern questions
+        type: "input",
+        message: "Intern's email: ",
+        name: "intEmail",
+      },
+      {
+        //intern questions
+        type: "input",
+        message: "Intern's school: ",
+        name: "intSchool",
       },
     ])
     .then((response) => {
+      const intName = `${response.intName}`;
+      const intId = `${response.intId}`;
+      const intEmail = `${response.intEmail}`;
+      const intSchool = `${response.intSchool}`;
+
+      const intern = new Intern(intName, intId, intEmail, intSchool);
       // add new intern to employees array
-      // promptForNextEmployee
+      team.push(intern);
+
+      promptForNextEmployee();
     });
 };
 
-const buildPage = () => {
-  // render(myArrayOfTeamMembers)
-};
+// const buildPage = () => {
+//   // render(myArrayOfTeamMembers)
+//   console.log("Yay!");
+// };
